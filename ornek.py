86dfe0d6e8d4f888a0e004de978d165234741c47
tayfunulu <tayfunulu@yahoo.com>
2016-05-24 17:19:30 +0300

    Update ornek.py

--- ornek.py
+++ ornek.py
@@ -1,9 +1,5 @@
 from DovizKur import DovizKurlari
-
 ornek = DovizKurlari()
-
 print "EURO DEGERİ="+ornek.DegerSor("EUR",4)
-
 Dolar_Deger = ornek.DegerSor("USD",4)
-
 print "DOLAR DEGERİ="+Dolar_Deger
