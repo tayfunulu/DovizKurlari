55b54cd786918d2fa63b12d6b3f6ccaa1eb5f6b7
tayfunulu <tayfunulu@yahoo.com>
2016-06-05 13:44:03 +0300

    Arsiv eklendi

--- DovizKurlari.py
+++ DovizKurlari.py
@@ -6,39 +6,62 @@
 import xml.etree.ElementTree as ET
 import sys
 if sys.version_info.major >2 :
-    from urllib.request import urlopen
-else : 
-    import urllib2
+	from urllib.request import urlopen
+else :
+	import urllib2
 
 class DovizKurlari():
 
-    def __init__(self):
-        self.__veri_update()
+	def __init__(self):
+        	pass
 
-    def __veri_update(self):
-        if sys.version_info.major >2 :
-            tree = ET.parse(urlopen('http://www.tcmb.gov.tr/kurlar/today.xml'))
-        else :
-            tree = ET.parse(urllib2.urlopen('http://www.tcmb.gov.tr/kurlar/today.xml'))
-        root = tree.getroot()
-        self.son={}
-        self.Kur_Liste=[]
-        for kurlars in root.findall('Currency'):
-            Kod= kurlars.get('Kod')
-            Unit = kurlars.find('Unit').text #    <Unit>1</Unit>
-            isim = kurlars.find('Isim').text #    <Isim>ABD DOLARI</Isim>
-            CurrencyName = kurlars.find('CurrencyName').text #    <CurrencyName>US DOLLAR</CurrencyName>
-            ForexBuying = kurlars.find('ForexBuying').text #    <ForexBuying>2.9587</ForexBuying>
-            ForexSelling = kurlars.find('ForexSelling').text #    <ForexSelling>2.964</ForexSelling>
-            BanknoteBuying = kurlars.find('BanknoteBuying').text #    <BanknoteBuying>2.9566</BanknoteBuying>
-            BanknoteSelling = kurlars.find('BanknoteSelling').text #    <BanknoteSelling>2.9684</BanknoteSelling>
-            CrossRateUSD = kurlars.find('CrossRateUSD').text #    <CrossRateUSD>1</CrossRateUSD>
-            self.Kur_Liste.append(Kod)
-            self.son [Kod] = [Kod,isim,CurrencyName,ForexBuying,ForexSelling,BanknoteBuying,BanknoteSelling,CrossRateUSD]
-        return self.son
+	def __veri_update(self,zaman="Bugun"):
+		if zaman == "Bugun":
+			self.url="http://www.tcmb.gov.tr/kurlar/today.xml"
+		else:
+			self.url=zaman
 
-    def DegerSor (self,sor,sor2):
-            return self.son[sor][sor2]
+		if sys.version_info.major >2 :
+			tree = ET.parse(urlopen(self.url))
+		else :
+			tree = ET.parse(urllib2.urlopen(self.url))
+
+		root = tree.getroot()
+		self.son={}
+		self.Kur_Liste=[]
+
+		for kurlars in root.findall('Currency'):
+			Kod= kurlars.get('Kod')
+			Unit = kurlars.find('Unit').text #    <Unit>1</Unit>
+			isim = kurlars.find('Isim').text #    <Isim>ABD DOLARI</Isim>
+			CurrencyName = kurlars.find('CurrencyName').text #    <CurrencyName>US DOLLAR</CurrencyName>
+			ForexBuying = kurlars.find('ForexBuying').text #    <ForexBuying>2.9587</ForexBuying>
+			ForexSelling = kurlars.find('ForexSelling').text #    <ForexSelling>2.964</ForexSelling>
+			BanknoteBuying = kurlars.find('BanknoteBuying').text #    <BanknoteBuying>2.9566</BanknoteBuying>
+			BanknoteSelling = kurlars.find('BanknoteSelling').text #    <BanknoteSelling>2.9684</BanknoteSelling>
+			CrossRateUSD = kurlars.find('CrossRateUSD').text #    <CrossRateUSD>1</CrossRateUSD>
+			self.Kur_Liste.append(Kod)
+			self.son [Kod] = [Kod,isim,CurrencyName,ForexBuying,ForexSelling,BanknoteBuying,BanknoteSelling,CrossRateUSD]
+
+		return self.son
+
+	def DegerSor (self,sor,sor2):
+		self.__veri_update()
+		return self.son[sor][sor2]
+
+	def Arsiv (self,sor,sor2,Gun,Ay,Yil):
+		self.__veri_update(self.__Url_Yap(Gun,Ay,Yil))
+		return self.son[sor][sor2]
+
+	def __Url_Yap (self,Gun,Ay,Yil):
+		if len (str(Gun)) == 1 :
+			Gun="0"+str(Gun)
+		if len (str(Ay)) == 1 :
+			Ay="0"+str(Ay)
+
+
+		self.url = ("http://www.tcmb.gov.tr/kurlar/"+str(Yil)+str(Ay)+"/"+str(Gun)+str(Ay)+str(Yil)+".xml")
+		return self.url
 
 #Ornek Kullanım için
 #from DovizKur import DovizKurlari
--- ornek.py
+++ ornek.py
@@ -1,5 +1,11 @@
 from DovizKurlari import DovizKurlari
 ornek = DovizKurlari()
+print ("Bugunun Kurlar : ")
 print ("EURO DEGERI="+ornek.DegerSor("EUR",4))
 Dolar_Deger = ornek.DegerSor("USD",4)
+print ("DOLAR DEGERI="+Dolar_Deger)
+
+print ("\nArsiv'deki bir degere bakalim.")
+print ("EURO DEGERI="+ornek.Arsiv("EUR",4,10,02,2015))
+Dolar_Deger = ornek.Arsiv("USD",4,10,02,2015)
 print ("DOLAR DEGERI="+Dolar_Deger)
