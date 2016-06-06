979ecf29779ed079c68cd4e248fd6c3c7760fbf2
Tayfun ULU <tayfunulu@yahoo.com>
2016-06-06 15:37:52 +0300

    Update ornek.py

--- ornek.py
+++ ornek.py
@@ -1,12 +1,16 @@
 from DovizKurlari import DovizKurlari
+# ornek isimli nesne yaratiliyor. 
 ornek = DovizKurlari()
 print ("Bugunun Kurlar : ")
 print ("EURO DEGERI="+ornek.DegerSor("EUR",4))
+# deger sor fonksiyonu ile USD'nin degeri sorgulaniyor. 
 Dolar_Deger = ornek.DegerSor("USD",4)
 print ("DOLAR DEGERI="+Dolar_Deger)
 
+# Arsiv konusu
 print ("\nArsiv'deki bir degere bakalim. 02.02.2015")
 print ("EURO DEGERI="+ornek.Arsiv("EUR",4,2,2,2015))
+# 02.02.2015 tarihindeki USD'in degeri sorgulaniyor. 
 Dolar_Deger = ornek.Arsiv("USD",4,2,2,2015)
 print ("DOLAR DEGERI="+Dolar_Deger)
 
